13d95fd70afb64397c1951e160e30b75b617d6d6
akshadaunde <akshadaunde06@gmail.com>
2023-07-14 23:40:49 +0530

    5 new javaproject commit

--- myjavafile1.java
+++ myjavafile1.java
@@ -1,5 +1,5 @@
 class Simple{
   public static void main(String args[]){
-  System.out.println("HELLO MY JAVA WORLD!!!!!WELCOME");
+  System.out.println("HELLO MY JAVA WORLD!!!!!WELCOME**************");
  }
 }
